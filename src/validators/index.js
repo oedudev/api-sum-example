c595da99ad4addb80d33b40acc6fa2360ad8311f
Eduardo Andrade <eduardo@andrade.com>
2020-02-09 16:22:47 -0300

    Implementing new validations

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,8 +1,29 @@
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 module.exports = {
     solve : [
-        check('a').isArray().withMessage("Propriedade 'a' não é um array"),
-        check('k').isNumeric().withMessage("Propriedade 'k' não é numérica")
+        // check('a').isArray().withMessage("Propriedade 'a' não é um array"),
+        check('k').isNumeric().withMessage("Propriedade 'k' não é numérica"),
+        body("a").custom((value, { req }) => {
+            const numberList = req.body.a
+
+            if(!Array.isArray(numberList)) {
+                throw new Error("Propriedade 'a' não é um array")
+            }
+
+            if (!numberList || numberList.length == 0) {
+                throw new Error("Propriedade 'a' veio vazia")
+            }
+
+            for (let idx = 0; idx < numberList.length; idx++) {
+                const element = numberList[idx];
+
+                if(!Number.isInteger(element)) {
+                    throw new Error(`Propriedade 'a' tem um elemento de valor '${element}' que não é um número inteiro`)
+                }
+                
+            }
+            
+        })
     ]
 }
