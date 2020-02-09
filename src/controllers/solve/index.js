192f4f4094b48df580ecf5bd551b57a9e4be1ded
Eduardo Andrade <eduardo@andrade.com>
2020-02-09 13:48:10 -0300

    Service layer implemented

--- src/controllers/solve/index.js
+++ src/controllers/solve/index.js
@@ -3,14 +3,13 @@ const solveService = require("../../service/solve")
 module.exports = {
     solve : async function(req, res) {
         const { a, k } = req.body
-
-        
+        const resolution = solveService.solve(a,k)
         
         res.json({
             status: "OK",
             a,
             k,
-            solve: solveService.solve(a,k)
+            resolution
         })
     }
 }
--- src/service/solve/index.js
+++ src/service/solve/index.js
@@ -1,5 +1,25 @@
 module.exports = {
     solve : function(numberList, resultDesired) {
-        return true
+
+        /* 
+            Para verificar se a soma dos itens corresponde ao resultado desejado, fazemos o seguinte:
+            - Para cada posição do array, pegamos o número e fazemos a soma com todos os números adiante
+            - Ao Obter o resultado, retorna true
+            - Ao final do loop, retorna false (pois não encontrou)
+        */
+        for (let firstIdx = 0; firstIdx < numberList.length -1; firstIdx++) {
+            for(let secondIdx=firstIdx+1; secondIdx < numberList.length; secondIdx++ ) {
+
+                const firstNumber = numberList[firstIdx];
+                const secondNumber = numberList[secondIdx]
+                const currentResult = firstNumber + secondNumber
+                
+                if (currentResult == resultDesired) {
+                    return true
+                }
+            }
+        }
+
+        return false
     }
 }
